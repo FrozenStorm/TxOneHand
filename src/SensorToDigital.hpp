18e4a5237353a1b4ea64bcc7612bf0fdd72ec899
Daniel <daniel.zwygart@hotmail.com>
2025-03-09 22:16:09 +0100

    Sensordaten an neue Hardware angepasst

--- src/SensorToDigital.hpp
+++ src/SensorToDigital.hpp
@@ -34,17 +34,17 @@ void SensorToDigital::doFunction()
     sensors_event_t accel, gyro, temp;
     mpu->getEvent(&accel, &gyro, &temp); // 3ms
 
-    radioData.rawData.gyroPitch = gyro.gyro.z;
-    radioData.rawData.gyroRoll = gyro.gyro.y;
-    radioData.rawData.gyroYaw = gyro.gyro.x;
-    radioData.rawData.accelPitch = -accel.acceleration.z;
-    radioData.rawData.accelRoll = -accel.acceleration.y;
-    radioData.rawData.accelYaw = -accel.acceleration.x;
+    radioData.rawData.gyroPitch = -gyro.gyro.z;
+    radioData.rawData.gyroRoll = gyro.gyro.x;
+    radioData.rawData.gyroYaw = -gyro.gyro.y;
+    radioData.rawData.accelPitch = -accel.acceleration.x;
+    radioData.rawData.accelRoll = -accel.acceleration.z;
+    radioData.rawData.accelYaw = accel.acceleration.y;
 
-    radioData.analogData.gyroPitch = -radioData.rawData.gyroPitch * 180 / PI; // Umrechnen in Grad/s
+    radioData.analogData.gyroPitch = radioData.rawData.gyroPitch * 180 / PI; // Umrechnen in Grad/s
     radioData.analogData.gyroRoll = radioData.rawData.gyroRoll * 180 / PI;
-    radioData.analogData.accelPitch = atan2(radioData.rawData.accelRoll, radioData.rawData.accelYaw) * 180 / PI;
-    radioData.analogData.accelRoll = atan2(radioData.rawData.accelPitch, radioData.rawData.accelYaw) * 180 / PI;
+    radioData.analogData.accelPitch = atan2(radioData.rawData.accelPitch, radioData.rawData.accelYaw) * 180 / PI;
+    radioData.analogData.accelRoll = atan2(radioData.rawData.accelRoll, radioData.rawData.accelYaw) * 180 / PI;
     // TODO radioData.analogData.accelYaw = .........
 
     radioData.analogData.pitch = filterRate * (radioData.analogData.pitch + radioData.analogData.gyroPitch * gyroRate) + (1 - filterRate) * radioData.analogData.accelPitch;
